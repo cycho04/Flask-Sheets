c817a6d61cd48ef394eedb928c0732a0e8788239
Chris Cho <chrisycho04@gmail.com>
2019-06-26 23:46:47 -0700

    added new form, works. need to clear values

--- apps/app.py
+++ apps/app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, flash, Response, redirect, url_for
+from forms import NewEmployeeForm
+from sheets import MakeEmployee
+from secret import secret_key
 
 
 app = Flask(__name__)
+app.secret_key = secret_key
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    form = NewEmployeeForm()
+    if request.method == 'POST':
+        first = request.form.get('first')
+        last = request.form.get('last')
+        casino = request.form.get('casino')
+        title = request.form.get('title')
+        hired = request.form.get('hired')
+        EIN = request.form.get('EIN')
+        MakeEmployee(first, last, casino, title, hired, EIN)
+        if form.validate() == False:
+            flash('Please fill out the marked sections')
+            return render_template('home.html', form=form)
+        elif form.validate_on_submit():
+
+            return redirect(url_for('new'))
+    elif request.method == 'GET':
+        return render_template('home.html', form=form)
 
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
--- apps/forms.py
+++ apps/forms.py
@@ -1,10 +1,15 @@
 from flask_wtf import Form
-from wtforms import StringField, DateField, IntegerField, RadioField, SubmitField
+from wtforms import StringField, DateTimeField, IntegerField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class NewEmployeeForm(Form):
     first = StringField('First', validators=[DataRequired()])
     last = StringField('Last', validators=[DataRequired()])
-    casino = RadioField('Casino', choices=[
-                        {'Empire': 'Empire'}, {'Lodi': 'Lodi'}])
+    casino = SelectField('Casino', choices=[
+        {'empire', 'Empire'}, {'lodi', 'Lodi'}])
+    title = SelectField('Title', choices=[{'associate', 'Associate'}, {
+        'supervisor', 'Supervisor'}])
+    hired = DateTimeField('Hired Date')
+    EIN = IntegerField('EIN')
+    submit = SubmitField('Submit')
